fd2f34ad9918232faab691a173bcbbf0952ef70c
Kishori7 <yourkiss08@gmail.com>
2024-10-14 11:40:35 +0700

    Add contact page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,5 +30,6 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
  <script
 src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+<a href="/contact">Contact Us</a>
 </body>
 </html>
